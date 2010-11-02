b895bdb2bac0d20bedb44ef61882cc5b001b16a8
Brett Rasmussen <brett.rasmussen@twoedge.com>
2010-11-01 18:41:43 -0600

    played around with addClass() and removeClass()

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,15 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-$(document).ready(function(){
-  $("a").click(function(event){
+$(document).ready(function() {
+  $('a').click(function(event) {
     alert("you've clicked a link");
+    event.preventDefault();
+  });
+
+  $('#border-switch-a').click(function(event) {
+    $('#border-switch').removeClass("blue");
+    $('#border-switch').addClass("red");
     event.preventDefault();
   });
 });
